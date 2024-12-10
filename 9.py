5db9f5c969cf603413e1a7dd63b39451a8032446
Christian Weymann <christian.weymann@bluewin.ch>
2024-12-10 17:07:22 +0100

    Day 9 part 2

--- 9.py
+++ 9.py
@@ -32,11 +32,20 @@ def build_disk_state_part1(initial_disk_state):
             right = disk_list.pop(-1)
         yield right
 
-def build_disk_state_part2(initial_disk_state):
-    pass
+def build_disk_state_part2(initial_disk_state, raw):
+    disk_list = list(initial_disk_state)
+    file_list = list(enumerate(get_files(raw)))
+    for file_name, size in file_list[::-1]:
+        file_position = disk_list.index(file_name)
+        for i in range(file_position):
+            if all(c == "." for c in disk_list[i:i+size]):
+                disk_list = [c if c != file_name else "." for c in disk_list]
+                disk_list[i:i+size] = [file_name]*size
+                break
+    return disk_list
 
 def get_checksum(disk_state):
-    return sum(i*file_id for i, file_id in enumerate(disk_state))
+    return sum(i*file_id for i, file_id in enumerate(disk_state) if file_id != ".")
 
 def part_1(used_input):
     files = get_files(used_input)
@@ -45,8 +54,10 @@ def part_1(used_input):
 
 
 def part_2(used_input):
-    pass
+    files = get_files(used_input)
+    spaces = get_spaces(used_input)
+    return get_checksum(build_disk_state_part2(get_initial_disk_state(files, spaces), used_input))
 
 
 print(part_1(data))
-print(part_2(example))
+print(part_2(data))
