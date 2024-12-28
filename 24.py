f4ca39e7b1361d0015208b27a6effd29afe617cf
Christian Weymann <christian.weymann@bluewin.ch>
2024-12-28 22:49:57 +0100

    Day 24 part 2

--- 24.py
+++ 24.py
@@ -1,5 +1,6 @@
 import re
 from collections import deque
+from itertools import permutations
 
 from aocd import get_data
 
@@ -61,6 +62,26 @@ y02: 0
 x00 AND y00 -> z00
 x01 XOR y01 -> z01
 x02 OR y02 -> z02"""
+example3 = """x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
+
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00"""
+
 
 def parse_wires(raw_wires):
     wires = {}
@@ -108,9 +129,51 @@ def part_1(used_input):
     return build_output(wires)
 
 
+def swap_gates(a, b, gates_dict):
+    gates_dict = gates_dict.copy()
+    gates_dict[a], gates_dict[b] = gates_dict[b], gates_dict[a]
+    return gates_dict
+
+
+def find_gate(a, b, op, gates_dict):
+    for gate, (c, d, ope) in gates_dict.items():
+        if op == ope and {a, b} == {c, d}:
+            return gate
+
+
 def part_2(used_input):
-    pass
+    input_wires, input_gates = parse_input(used_input)
+    gates_dict = {out: other for *other, out in input_gates}
+    previous_carry = find_gate("x00", "y00", "AND", gates_dict)
+    swapped = set()
+    for i in range(1, 45):
+        x_bit = f"x{i:02}"
+        y_bit = f"y{i:02}"
+        z_bit = f"z{i:02}"
+
+        total = find_gate(x_bit, y_bit, "XOR", gates_dict)
+        assert total is not None
+        if (actual_z := find_gate(total, previous_carry, "XOR", gates_dict)) is None:
+            for total_replace in gates_dict:
+                if find_gate(total_replace, previous_carry, "XOR", gates_dict) is not None:
+                    gates_dict = swap_gates(total, total_replace, gates_dict)
+                    swapped |= {total_replace, total}
+                    total = total_replace
+                    break
+        elif actual_z != z_bit:
+            gates_dict = swap_gates(actual_z, z_bit, gates_dict)
+            swapped |= {actual_z, z_bit}
+
+        new_carry_bits = find_gate(x_bit, y_bit, "AND", gates_dict)
+        assert new_carry_bits is not None
+        new_carry_total = find_gate(total, previous_carry, "AND", gates_dict)
+        assert new_carry_total is not None
+        new_carry = find_gate(new_carry_bits, new_carry_total, "OR", gates_dict)
+        assert new_carry is not None
+        previous_carry = new_carry
+
+    return ",".join(sorted(swapped))
 
 
 print(part_1(data))
-print(part_2(example))
+print(part_2(data))
